fa90dfe0e55ad2600c901a8bcfdc3abc2cb444a4
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2023-02-21 00:08:52 +0200

    chor: add history

---
+++ .history/src/App_20230221000455.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+
+function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://ramcis-testers-stars.firebaseio.com/cart.json", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cart),
+    });
+    return () => {
+      effect;
+    };
+  }, [input]);
+  return (
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
