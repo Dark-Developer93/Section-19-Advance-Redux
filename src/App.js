7e1c392735d5a585fa7b133873c6ce86468c1796
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2023-02-21 00:09:12 +0200

    Add: add the fetch method

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,20 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://ramcis-testers-stars.firebaseio.com/cart.json", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cart),
+    });
+    // return () => {
+    //   effect;
+    // };
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
